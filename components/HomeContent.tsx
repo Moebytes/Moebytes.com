25b7b23fcb3ff89631602986878685a3a661ca60
Moebytes <pie4chan@gmail.com>
2026-02-16 18:29:03 -0500

    just use skill icons

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,5 +1,72 @@
 import React from "react"
 import star from "../assets/svg/star.svg"
+
+import ae from "../assets/icons/ae.png"
+import ai from "../assets/icons/ai.png"
+import androidstudio from "../assets/icons/androidstudio.png"
+import angular from "../assets/icons/angular.png"
+import anki from "../assets/icons/anki.png"
+import blender from "../assets/icons/blender.png"
+import c from "../assets/icons/c.png"
+import clipstudio from "../assets/icons/clipstudio.png"
+import cpp from "../assets/icons/cpp.png"
+import csharp from "../assets/icons/csharp.png"
+import css from "../assets/icons/css.png"
+import djs from "../assets/icons/djs.png"
+import effectrix from "../assets/icons/effectrix.png"
+import electron from "../assets/icons/electron.png"
+import expo from "../assets/icons/expo.png"
+import figma from "../assets/icons/figma.png"
+import fl from "../assets/icons/fl.png"
+import glyphs from "../assets/icons/glyphs.png"
+import go from "../assets/icons/go.png"
+import godot from "../assets/icons/godot.png"
+import houdini from "../assets/icons/houdini.png"
+import html from "../assets/icons/html.png"
+import java from "../assets/icons/java.png"
+import js from "../assets/icons/js.png"
+import juce from "../assets/icons/juce.png"
+import kotlin from "../assets/icons/kotlin.png"
+import less from "../assets/icons/less.png"
+import live2d from "../assets/icons/live2d.png"
+import massive from "../assets/icons/massive.png"
+import maya from "../assets/icons/maya.png"
+import net from "../assets/icons/net.png"
+import next from "../assets/icons/next.png"
+import nodejs from "../assets/icons/nodejs.png"
+import npm from "../assets/icons/npm.png"
+import objc from "../assets/icons/objc.png"
+import omni from "../assets/icons/omni.png"
+import pg from "../assets/icons/pg.png"
+import phaseplant from "../assets/icons/phaseplant.png"
+import php from "../assets/icons/php.png"
+import pr from "../assets/icons/pr.png"
+import ps from "../assets/icons/ps.png"
+import python from "../assets/icons/python.png"
+import react from "../assets/icons/react.png"
+import redis from "../assets/icons/redis.png"
+import redux from "../assets/icons/redux.png"
+import rsbuild from "../assets/icons/rsbuild.png"
+import ruby from "../assets/icons/ruby.png"
+import rust from "../assets/icons/rust.png"
+import scrivener from "../assets/icons/scrivener.png"
+import scss from "../assets/icons/scss.png"
+import serum from "../assets/icons/serum.png"
+import sql from "../assets/icons/sql.png"
+import substance from "../assets/icons/substance.png"
+import svelte from "../assets/icons/svelte.png"
+import swift from "../assets/icons/swift.png"
+import tableplus from "../assets/icons/tableplus.png"
+import tailwind from "../assets/icons/tailwind.png"
+import ts from "../assets/icons/ts.png"
+import unity from "../assets/icons/unity.png"
+import vintageverb from "../assets/icons/vintageverb.png"
+import vscode from "../assets/icons/vscode.png"
+import vue from "../assets/icons/vue.png"
+import wasm from "../assets/icons/wasm.png"
+import webpack from "../assets/icons/webpack.png"
+import xcode from "../assets/icons/xcode.png"
+import zbrush from "../assets/icons/zbrush.png"
 import "./styles/homecontent.less"
 
 const HomeContent: React.FunctionComponent = () => {
@@ -11,89 +78,86 @@ const HomeContent: React.FunctionComponent = () => {
             </div>
             <div className="homecontent-text-container">
                 <span className="homecontent-text">
-                    I am a programmer from the United States with a B.S. degree in Computer Science and 
-                    1 year of experience. In my free time I make software and tools to enhance my life, 
-                    as well as create things related to my passions and interests. 
+                    I’m a programmer from the United States. In my free time I create software to 
+                    enhance my daily life, as well as create things that I am passionate about. 
+                    I have a B.S. degree in Computer Science and 1 year of experience. 
                     <br/><br/>
                     What is "moe"? Basically it means that I love cute things. As a word of caution, 
                     you are going to see a lot of moe things here. Why would you come to my website 
                     not expecting moe!?
                     <br/><br/>
                     Below is a list of tools I am familiar with, but it is not an exhaustive list 
-                    of everything I have ever used. 
+                    of everything I have used.
                 </span>
             </div>
             <div className="homecontent-header-container">
                 <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Design</span>
+                <span className="homecontent-header-text">Software & Tools</span>
             </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    Figma, Adobe Illustrator, Adobe Photoshop
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Web Frontend</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    TypeScript, React, Redux, HTML/CSS, SCSS/LESS
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Web Backend</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    Node.js, Express, PostgreSQL, Redis, Python, Next.js
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Desktop</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    TypeScript, Electron, React
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Mobile</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    TypeScript, React Native, Kotlin, Swift
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Discord Bots</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    TypeScript, Node.js, Discord.js
-                </span>
+            <div className="homecontent-icon-container">
+                <img className="homecontent-icon" src={ts} draggable={false}/>
+                <img className="homecontent-icon" src={js} draggable={false}/>
+                <img className="homecontent-icon" src={html} draggable={false}/>
+                <img className="homecontent-icon" src={css} draggable={false}/>
+                <img className="homecontent-icon" src={scss} draggable={false}/>
+                <img className="homecontent-icon" src={less} draggable={false}/>
+                <img className="homecontent-icon" src={wasm} draggable={false}/>
+                <img className="homecontent-icon" src={sql} draggable={false}/>
+                <img className="homecontent-icon" src={python} draggable={false}/>
+                <img className="homecontent-icon" src={cpp} draggable={false}/>
+                <img className="homecontent-icon" src={react} draggable={false}/>
+                <img className="homecontent-icon" src={redux} draggable={false}/>
+                <img className="homecontent-icon" src={electron} draggable={false}/>
+                <img className="homecontent-icon" src={djs} draggable={false}/>
+                <img className="homecontent-icon" src={juce} draggable={false}/>
+                <img className="homecontent-icon" src={nodejs} draggable={false}/>
+                <img className="homecontent-icon" src={npm} draggable={false}/>
+                <img className="homecontent-icon" src={pg} draggable={false}/>
+                <img className="homecontent-icon" src={redis} draggable={false}/>
+                <img className="homecontent-icon" src={webpack} draggable={false}/>
+                <img className="homecontent-icon" src={rsbuild} draggable={false}/>
+                <img className="homecontent-icon" src={vscode} draggable={false}/>
+                <img className="homecontent-icon" src={tableplus} draggable={false}/>
+                <img className="homecontent-icon" src={androidstudio} draggable={false}/>
+                <img className="homecontent-icon" src={xcode} draggable={false}/>
+                <img className="homecontent-icon" src={figma} draggable={false}/>
+                <img className="homecontent-icon" src={ps} draggable={false}/>
+                <img className="homecontent-icon" src={ai} draggable={false}/>
+                <img className="homecontent-icon" src={pr} draggable={false}/>
+                <img className="homecontent-icon" src={ae} draggable={false}/>
+                <img className="homecontent-icon" src={clipstudio} draggable={false}/>
+                <img className="homecontent-icon" src={live2d} draggable={false}/>
+                <img className="homecontent-icon" src={fl} draggable={false}/>
+                <img className="homecontent-icon" src={serum} draggable={false}/>
+                <img className="homecontent-icon" src={omni} draggable={false}/>
+                <img className="homecontent-icon" src={massive} draggable={false}/>
+                <img className="homecontent-icon" src={phaseplant} draggable={false}/>
+                <img className="homecontent-icon" src={effectrix} draggable={false}/>
+                <img className="homecontent-icon" src={vintageverb} draggable={false}/>
+                <img className="homecontent-icon" src={anki} draggable={false}/>
             </div>
             <div className="homecontent-header-container">
                 <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Audio Plugins</span>
+                <span className="homecontent-header-text">I Might Survive If Forced To Use</span>
             </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    JUCE, C++, TypeScript, React
-                </span>
-            </div>
-            <div className="homecontent-header-container">
-                <img className="homecontent-header-icon" src={star} draggable={false}/>
-                <span className="homecontent-header-text">Game Mods</span>
-            </div>
-            <div className="homecontent-text-container">
-                <span className="homecontent-text">
-                    C++, Objective-C
-                </span>
+            <div className="homecontent-icon-container">
+                <img className="homecontent-icon" src={angular} draggable={false}/>
+                <img className="homecontent-icon" src={vue} draggable={false}/>
+                <img className="homecontent-icon" src={svelte} draggable={false}/>
+                <img className="homecontent-icon" src={next} draggable={false}/>
+                <img className="homecontent-icon" src={expo} draggable={false}/>
+                <img className="homecontent-icon" src={tailwind} draggable={false}/>
+                <img className="homecontent-icon" src={csharp} draggable={false}/>
+                <img className="homecontent-icon" src={net} draggable={false}/>
+                <img className="homecontent-icon" src={java} draggable={false}/>
+                <img className="homecontent-icon" src={kotlin} draggable={false}/>
+                <img className="homecontent-icon" src={swift} draggable={false}/>
+                <img className="homecontent-icon" src={objc} draggable={false}/>
+                <img className="homecontent-icon" src={ruby} draggable={false}/>
+                <img className="homecontent-icon" src={php} draggable={false}/>
+                <img className="homecontent-icon" src={c} draggable={false}/>
+                <img className="homecontent-icon" src={rust} draggable={false}/>
+                <img className="homecontent-icon" src={go} draggable={false}/>
             </div>
             <div className="homecontent-text-container">
                 <span className="homecontent-text">
