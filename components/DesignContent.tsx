84a09e23f578aad157eb74dd10fcc7d5be6c22a1
Moebytes <pie4chan@gmail.com>
2026-03-10 13:51:53 -0400

    forgot to update

--- components/DesignContent.tsx
+++ components/DesignContent.tsx
@@ -4,8 +4,8 @@ import moepictures from "../assets/design/moepictures.png"
 import moepicturesApp from "../assets/design/moepictures-app.png"
 import kisaragi from "../assets/design/kisaragi.png"
 import tunePlayer from "../assets/design/tune-player.png"
-import picPlayer from "../assets/design/pic-player.png"
-import motionPlayer from "../assets/design/motion-player.png"
+import picDisplay from "../assets/design/pic-display.png"
+import framePlayer from "../assets/design/frame-player.png"
 import "./styles/designcontent.less"
 
 const DesignContent: React.FunctionComponent = () => {
@@ -23,19 +23,19 @@ const DesignContent: React.FunctionComponent = () => {
         window.open("https://www.figma.com/design/CTEs64SQjKg7M1SXzGx0Kh/Tune-Player", "_blank")
     }
 
-    const openPicPlayer = () => {
-        window.open("https://www.figma.com/design/kqGaBzYxe93zSPxDPz6wRa/Pic-Player", "_blank")
+    const openPicDisplay = () => {
+        window.open("https://www.figma.com/design/kqGaBzYxe93zSPxDPz6wRa/Pic-Display", "_blank")
     }
 
-    const openMotionPlayer = () => {
-        window.open("https://www.figma.com/design/PpYPQAYojONPWedMbDRL8t/Motion-Player", "_blank")
+    const openFramePlayer = () => {
+        window.open("https://www.figma.com/design/PpYPQAYojONPWedMbDRL8t/Frame-Player", "_blank")
     }
 
     return (
         <div className="design-content">
             <img className="design-content-img" src={tunePlayer} draggable={false} style={{cursor: "pointer"}} onClick={openTunePlayer}/>
-            <img className="design-content-img" src={picPlayer} draggable={false} style={{cursor: "pointer"}} onClick={openPicPlayer}/>
-            <img className="design-content-img" src={motionPlayer} draggable={false} style={{cursor: "pointer"}} onClick={openMotionPlayer}/>
+            <img className="design-content-img" src={picDisplay} draggable={false} style={{cursor: "pointer"}} onClick={openPicDisplay}/>
+            <img className="design-content-img" src={framePlayer} draggable={false} style={{cursor: "pointer"}} onClick={openFramePlayer}/>
             <img className="design-content-img" src={moepictures} draggable={false} style={{cursor: "pointer"}} onClick={openMoepictures}/>
             <img className="design-content-img" src={moepicturesApp} draggable={false} style={{cursor: "pointer", height: mobile ? "700px" : "1000px"}} onClick={openMoepicturesApp}/>
             <img className="design-content-img" src={kisaragi} draggable={false}/>
